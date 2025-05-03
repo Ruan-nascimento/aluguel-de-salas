67e1287d755817819ea587c70753658af753aa3b
Ruan Carlos <ruan.nascimento019@gmail.com>
2025-05-02 22:18:29 -0300

    tutorial

--- src/interfaces/main_window.py
+++ src/interfaces/main_window.py
@@ -3,6 +3,4 @@ from PyQt5.QtWidgets import QMainWindow
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
-        
-        
+ 
--- src/main.py
+++ src/main.py
@@ -2,8 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from interfaces.main_window import MainWindow
 
-def main():
-    
+def main():   
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
@@ -13,5 +12,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-
     
